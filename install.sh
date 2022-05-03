2354497c06ffcdcfd0e0262bb731a30af7d9f5be
Karan <90849102+KaranGamerz207@users.noreply.github.com>
2022-05-03 20:40:41 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,51 @@
+#!/bin/bash
+# Init
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+#....
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "Please Run This Script As A ROOT User" 1>&2
+   exit 1
+fi
+# ...
 
+
+echo "updating Your tmp files"
+apt update -y
+apt upgrade -y
+apt-get update -y
+apt-get upgrade -y
+apt install zip unzip -y
+apt install figlet toilet -y
+clear
+figlet -c Life is EZ Now
+
+echo ""
+echo "* RDP Installer"
+echo "*"
+echo "* Copyright (C) 2022 - 2022, DNxRG Development, <discord.gg/dnxrg>"
+echo "*"
+echo "* Made for Crystal Hosting"
+echo "*"
+echo "*"
+echo "########################################################################################################"
+echo "*                                                                                                      $"
+echo "* [1] Install RDP With Xrdp.                                                                           $"
+echo "* [2] Create New User.                                                                                 $"
+echo "*                                                                                                      $"
+echo "########################################################################################################"
+echo ""
+echo ""
+
+read -p "Enter Input: " input
+case $input in  
+    1) bash <(curl -s https://raw.githubusercontent.com/NotRoyadma/ptero_database_repo_main/main/swp.sh);;
+    2) bash <(curl -s https://raw.githubusercontent.com/NotRoyadma/ptero_database_repo_main/main/database.sh);;
+*) exit;;
+echo "This is Made For Crystal Hosting, Discord - discord.gg/vpsfree"
+
+sudo su - 
+sudo apt install xrdp
+sudo systemctl enable --now xrdp
+sudo ufw allow from any to any port 3389 proto tcp

44a7769c74a4c3b7fb45b29a134320b01ba09c09
Karan <90849102+KaranGamerz207@users.noreply.github.com>
2022-05-03 21:04:59 +0530

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -4,3 +4,7 @@ sudo su -
 sudo apt install xrdp
 sudo systemctl enable --now xrdp
 sudo ufw allow from any to any port 3389 proto tcp
+echo 
+say @B"Your RDP is Installed. You Now Need to Create a User. Run Script again to create." green
+echo 
+exit 1

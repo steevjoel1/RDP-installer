e57e2eb5461554a7910b464c52bda023b227a0c9
Karan <90849102+KaranGamerz207@users.noreply.github.com>
2022-05-03 23:11:22 +0530

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-# Init
-FILE="/tmp/out.$$"
-GREP="/bin/grep"
-#....
-# Make sure only root can run our script
-if [ "$(id -u)" != "0" ]; then
-   echo "Please Run This Script As A ROOT User" 1>&2
-   exit 1
-fi
-# ...
-
 sudo su - 
 figlet -c RDP Installer
 sudo apt install xrdp

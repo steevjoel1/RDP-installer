99e987aff13b496d634b48910cf9a8fdd51ef4d5
Karan <90849102+KaranGamerz207@users.noreply.github.com>
2022-05-03 22:45:55 +0530

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+# Init
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+#....
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "Please Run This Script As A ROOT User" 1>&2
+   exit 1
+fi
+# ...
+
 figlet -c RDP Installer
 sudo su - 
 sudo apt install xrdp
 sudo systemctl enable --now xrdp
 sudo ufw allow from any to any port 3389 proto tcp 
 clear
-
 echo "RDP Is Installed, Run Script Again and Choose option 2"

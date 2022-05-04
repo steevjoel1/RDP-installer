e7a4eecd145463f34bd7d521f028e43423946047
Karan <90849102+KaranGamerz207@users.noreply.github.com>
2022-05-04 08:31:00 +0530

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 figlet -c RDP Installer
+echo -e "\e[40;38;5;82m Installing RDP Files..\e[0m"
 sudo apt install xrdp
 sudo systemctl enable --now xrdp
 sudo ufw allow from any to any port 3389 proto tcp

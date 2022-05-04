ffce86da0d83cef9d341088c21f0e48b2dd2a385
Karan <90849102+KaranGamerz207@users.noreply.github.com>
2022-05-04 10:18:16 +0530

    Update create user.sh

--- create user.sh
+++ create user.sh
@@ -2,5 +2,5 @@
 figlet -c Create User
 echo -e "Downloading User Config...."
 echo -e "Files Downloaded"
-echo -e "Run \e[40;38;5;82m adduser\e[0m"
+echo -e "Run \e[40;38;5;82m adduser <username>\e[0m"
 exit 1

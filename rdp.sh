9649cb2684ca2465c6bf35d4ce0d740f32f099a4
steevjoel1 <93113285+steevjoel1@users.noreply.github.com>
2022-05-05 15:05:36 +0530

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -3,5 +3,5 @@ figlet -c RDP Installer
 echo -e "\e[40;38;5;82m Installing RDP Files..\e[0m"
 sudo apt install xrdp
 sudo systemctl enable --now xrdp
-sudo ufw allow from any to any port 3389 proto tcp
+sudo ufw allow from any to any port 3845 proto tcp
 echo -e "\e[40;38;5;82m RDP Is Installed, Run Script Again and Choose option 2\e[0m"
